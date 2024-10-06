100cc9be412fedaf8269b921c2572a815c9aab39
kvamca <kvamca@gmail.com>
2024-10-06 17:51:52 +0200

    Game and czechitas course finished

--- 06-pig-game-start/app.js
+++ 06-pig-game-start/app.js
@@ -3,28 +3,89 @@ let pointsInOneRound;
 let activePlayer;
 let dice;
 let finalPoints;
+let gameRunning;
 
-points = [0, 0];
-pointsInOneRound = 0;
-activePlayer = 0;
 finalPoints = 25;
 
+function init(){
+    points = [0,0];
+    activePlayer = 0;
+    pointsInOneRound = 0;
+    gameRunning = true;
 
-document.getElementById("body-0").textContent = "0";
-document.getElementById("body-1").textContent = "0";
-document.getElementById("soucasne-0").textContent = "0";
-document.getElementById("soucasne-1").textContent = "0";
+    document.querySelector(".kostka").style.display = "none";
 
+    document.getElementById("body-0").textContent = "0";
+    document.getElementById("body-1").textContent = "0";
+    document.getElementById("soucasne-0").textContent = "0";
+    document.getElementById("soucasne-1").textContent = "0";
+
+    document.querySelector("#jmeno-0").textContent = "Hráč 1";
+    document.querySelector("#jmeno-1").textContent = "Hráč 2";
+
+    document.querySelector(".hrac-0-panel").classList.remove("aktivni");
+    document.querySelector(".hrac-1-panel").classList.remove("aktivni");
+    document.querySelector(".hrac-0-panel").classList.remove("vitez");
+    document.querySelector(".hrac-1-panel").classList.remove("vitez");
+    document.querySelector(".hrac-0-panel").classList.add("aktivni");
+
+};
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    pointsInOneRound = 0;
+    document.getElementById("soucasne-0").textContent = "0";
+    document.getElementById("soucasne-1").textContent = "0";
+
+    document.querySelector(".hrac-0-panel").classList.toggle("aktivni");
+    document.querySelector(".hrac-1-panel").classList.toggle("aktivni");
+    
+}
+
+init();
 
 document.querySelector(".kostka").style.display = "none";
 
 document.querySelector(".tlacitko-hod").addEventListener("click", function(){
-    // 1. Random number
-    dice = Math.ceil(Math.random()*6);
-    //2. Show in game
-    let diceDOM = document.querySelector(".kostka");
+if (gameRunning) {
+
+    dice = Math.ceil(Math.random()*6);                   //1. Random number
+    let diceDOM = document.querySelector(".kostka");     //2. Show in game
     diceDOM.style.display = "block";
     diceDOM.textContent = dice;
-    //3. Refresh points if there is (not) 1
     
+    if (dice !== 1) {                                   //3. Refresh points if there is (not) 1
+    pointsInOneRound += dice;                           //4. Update points for player
+    document.querySelector("#soucasne-" + activePlayer).textContent = pointsInOneRound;
+    } else { 
+            nextPlayer();
+    }
+    
+} else {
+    
+}
+    });
+
+document.querySelector(".tlacitko-dost").addEventListener("click", function () {
+if (gameRunning) {
+    points[activePlayer] += pointsInOneRound;
+
+    document.querySelector("#body-" + activePlayer).textContent = points[activePlayer];
+
+    if (points[activePlayer] >= finalPoints) {
+        document.querySelector("#jmeno-" + activePlayer).textContent = "Winner!";
+        document.querySelector(".hrac-" + activePlayer + "-panel").classList.remove("aktivni");
+        document.querySelector(".hrac-" + activePlayer + "-panel").classList.add("vitez");
+        document.querySelector(".kostka").style.display = "none";
+        gameRunning = false;
+        
+    } else {
+        nextPlayer();
+    }
+    
+} else {
+    
+}       
 });
+
+document.querySelector(".tlacitko-novy").addEventListener("click", init);
